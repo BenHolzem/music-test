408b8086e991c371c8f850e92fe4c3995c10c7ca
unknown <holzem.ben@gmail.com>
2021-06-05 11:20:37 -0500

    add url

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const {
   } = process.env;
   
 const handleLogin = () => {
-    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true&scope=user-top-read`;
+    window.location = `https://accounts.spotify.com/authorize?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true&scope=user-top-read`;
   };
 
 const styles = () => ({
